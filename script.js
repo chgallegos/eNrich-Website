53b07e43a3c267a2c8286415b821f1dd4bfa967f
ChrisG <chrisale89@gmail.com>
2023-06-21 15:29:29 -0600

    final revision

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 const submitButton = document.getElementById('submit-button');
 const messageInput = document.getElementById('message');
-const responseContainer = document.querySelector('.response-container');
+const responseContainer = document.getElementById('response-container');
+const responseElement = document.getElementById('response');
+const resetButton = document.createElement('button');
+resetButton.id = 'reset-button';
+resetButton.className = 'submit-button-red'; // Add CSS class for red button style
+resetButton.textContent = 'Ask Another Question';
+resetButton.addEventListener('click', handleReset);
 
 // Replace 'YOUR_API_KEY' with your actual OpenAI API key
-const apiKey = 'YOUR_API_KEY';
+const apiKey = '';
 const engine = 'text-davinci-003';
 
 submitButton.addEventListener('click', handleSubmit);
 messageInput.addEventListener('keyup', handleKeyUp);
 
+const loadingMessage = "Chill out. I'm thinking";
+const loadingAnimationDelay = 500; // Delay between each dot (in milliseconds)
+let loadingAnimationInterval;
+
 function handleSubmit() {
   const userInput = messageInput.value.trim();
   if (userInput === '') return;
 
-  // Disable the submit button and display loading text
-  submitButton.disabled = true;
-  submitButton.value = 'Loading...';
+  // Hide the input container
+  messageInput.style.display = 'none';
+  submitButton.style.display = 'none';
+
+  // Show loading message with typewriter effect
+  typeWriterLoadingMessage(loadingMessage);
 
   // Make the API call to OpenAI
   makeOpenAIAPIRequest(userInput)
     .then(answer => {
       console.log('API Response:', answer);
-      replaceInputWithResponse(answer);
+      stopLoadingAnimation();
+      typeWriter(answer);
     })
     .catch(error => {
       console.error('Error:', error);
+      stopLoadingAnimation();
       replaceInputWithResponse('Sorry, something went wrong. Please try again.');
-    })
-    .finally(() => {
-      // Enable the submit button and change the text back to 'Submit'
-      submitButton.disabled = false;
-      submitButton.value = 'Submit';
     });
 }
 
@@ -69,28 +79,78 @@ function makeOpenAIAPIRequest(input) {
     });
 }
 
+function typeWriterLoadingMessage(message) {
+  let i = 0;
+  const typingDelay = 100; // Adjust the delay for faster typing
+
+  responseElement.textContent = ''; // Clear existing content
+  responseElement.style.fontSize = '18px'; // Adjust the font size as needed
+  responseElement.style.textAlign = 'center'; // Center the text
+
+  function typeNextChar() {
+    if (i < message.length) {
+      responseElement.textContent += message.charAt(i);
+      i++;
+      setTimeout(typeNextChar, typingDelay);
+    } else {
+      // Start the loading animation after typing is complete
+      startLoadingAnimation();
+    }
+  }
+
+  typeNextChar();
+}
+
+function typeWriter(text) {
+  let i = 0;
+  const typingDelay = 20; // Adjust the delay for faster typing
+
+  responseElement.textContent = ''; // Clear existing content
+  responseElement.style.fontSize = '18px'; // Adjust the font size as needed
+  responseElement.style.textAlign = 'center'; // Center the text
+
+  function typeNextChar() {
+    if (i < text.length) {
+      responseElement.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeNextChar, typingDelay);
+    } else {
+      // Display 'Ask Another Question' button after typing is complete
+      responseContainer.appendChild(resetButton);
+    }
+  }
+
+  typeNextChar();
+}
+
 function replaceInputWithResponse(response) {
-  const responseElement = document.createElement('div');
-  responseElement.className = 'response';
   responseElement.textContent = response;
-
-  const resetButton = document.createElement('button');
-  resetButton.id = 'reset-button';
-  resetButton.className = 'submit-button-red'; // Add CSS class for red button style
-  resetButton.textContent = 'Ask Another Question';
-  resetButton.addEventListener('click', handleReset);
-
-  messageInput.style.display = 'none';
-  submitButton.style.display = 'none';
-  responseContainer.innerHTML = '';
-  responseContainer.appendChild(responseElement);
+  // Display 'Ask Another Question' button
   responseContainer.appendChild(resetButton);
 }
 
+function startLoadingAnimation() {
+  let dotCount = 0;
+  responseElement.textContent = loadingMessage;
+
+  loadingAnimationInterval = setInterval(() => {
+    dotCount = (dotCount + 1) % 4;
+    responseElement.textContent = loadingMessage + '.'.repeat(dotCount);
+  }, loadingAnimationDelay);
+}
+
+function stopLoadingAnimation() {
+  clearInterval(loadingAnimationInterval);
+}
+
 function handleReset() {
+  // Reset the form
   messageInput.value = '';
   messageInput.style.display = 'block';
   submitButton.style.display = 'block';
-  responseContainer.innerHTML = '';
-}
 
+  // Clear the response
+  responseElement.textContent = '';
+  responseContainer.innerHTML = '';
+  stopLoadingAnimation();
+}
